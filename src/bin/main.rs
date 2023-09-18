47912ae9d093dfad6fcbe37c09ef9c18211ec526
sillysillyman <sillysillyman@sillysillyman.local>
2023-09-18 17:22:57 +0900

    tutorial project finished

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,6 @@
+extern crate hello;
+use hello::ThreadPool;
+
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
@@ -7,12 +10,16 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+    println!("Shtting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -31,6 +38,7 @@ fn handle_connection(mut stream: TcpStream) {
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
+    
     let mut file = File::open(filename).unwrap();
     let mut contents = String::new();
 
